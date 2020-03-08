8c3bc2192b1999b1e1d8142e18a5bde01ad5ee1d
pablomdd <pablo.mdd.007@gmail.com>
2020-03-07 23:47:18 -0600

    Add ranges

---
+++ c21-ranges.py
@@ -0,0 +1,9 @@
+# Print all event numbers from 0 - 100
+
+# ---Remember range upper limit is exclusive---
+# for i in range(0,101,2):
+#     print(i)
+
+# Print all Odd numbers from 1 - 99
+for i in range(1,100,2):
+    print(i)
